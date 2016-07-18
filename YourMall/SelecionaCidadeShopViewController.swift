917fb44d2834f633dde43fe239b497f22a70cf00
Juan Munhoes Junior <Juan Munhoes Junior>
2016-07-18 15:08:55 -0300

    Add Controller Selectiona Cidade Shopping

---
+++ YourMall/SelecionaCidadeShopViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelecionaCidadeShopViewController.swift
+//  YourMall
+//
+//  Created by Student on 7/18/16.
+//  Copyright © 2016 Hackatruck. All rights reserved.
+//
+
+import UIKit
+
+class SelecionaCidadeShopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
